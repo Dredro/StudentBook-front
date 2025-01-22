3b42d764556962f13e5b68114c64ffcdc3c56b93
Kamil Gniazdowski <110768094+Dredro@users.noreply.github.com>
2025-01-22 14:58:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-
+import './App.css'
 const App = () => {
     const [posts, setPosts] = useState([]);
     const [newPost, setNewPost] = useState({ author: "", title: "", description: "", image: "" });
